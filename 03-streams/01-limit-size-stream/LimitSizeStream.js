a809691e9c7f359070e364819e26c88d749408ef
jnkbldgn <buldygin.em@gmail.com>
2022-03-26 23:02:04 +0300

    Задача №3.1

--- 03-streams/01-limit-size-stream/LimitSizeStream.js
+++ 03-streams/01-limit-size-stream/LimitSizeStream.js
@@ -4,10 +4,36 @@ const LimitExceededError = require('./LimitExceededError');
 class LimitSizeStream extends stream.Transform {
   constructor(options) {
     super(options);
+
+    this.size = 0;
+    this.limit = options.limit;
   }
 
   _transform(chunk, encoding, callback) {
+    const buffer = this.writableObjectMode ? this.objectToBuffer(chunk, encoding) : chunk;
 
+    this.size += buffer.length;
+
+    if (this.size > this.limit) {
+      callback(new LimitExceededError());
+    } else {
+      callback(null, buffer);
+    }
+  }
+
+  objectToBuffer(chunk, encoding) {
+    switch (typeof chunk) {
+      case 'string': {
+        return Buffer.from(chunk, encoding);
+      }
+      case 'function':
+      case 'undefined': {
+        return Buffer.from(String(chunk), encoding);
+      }
+      default: {
+        return Buffer.from(JSON.stringify(chunk), encoding);
+      }
+    }
   }
 }
 
