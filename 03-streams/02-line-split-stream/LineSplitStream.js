ed4bfde9c9f14f6c2e432efde843c31656645786
jnkbldgn <buldygin.em@gmail.com>
2022-03-27 04:42:17 +0300

    Задача №3.2

--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -4,12 +4,29 @@ const os = require('os');
 class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
+
+    this.lastEl = '';
   }
 
-  _transform(chunk, encoding, callback) {
+  async _transform(chunk, encoding, callback) {
+    const text = chunk.toString();
+    const matches =(this.lastEl + text).split(os.EOL).filter((it) => !!it);
+
+    if (text[text.length - 1] !== os.EOL) {
+      this.lastEl = matches.pop();
+    }
+
+    matches.forEach((it) => this.push(it));
+
+    callback();
   }
 
   _flush(callback) {
+    if (!!this.lastEl) {
+      this.push(this.lastEl);
+      this.lastEl = '';
+    }
+    callback();
   }
 }
 
