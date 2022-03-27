fd88d41dbf57384ebaec3e21ce955d2f8881b1af
jnkbldgn <buldygin.em@gmail.com>
2022-03-27 21:34:54 +0300

    Задача №3.3

--- 03-streams/03-file-server-get/server.js
+++ 03-streams/03-file-server-get/server.js
@@ -1,6 +1,12 @@
-const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
+const {constants: {
+  HTTP_STATUS_BAD_REQUEST,
+  HTTP_STATUS_NOT_FOUND,
+  HTTP_STATUS_INTERNAL_SERVER_ERROR,
+  HTTP_STATUS_NOT_IMPLEMENTED,
+}} = require('http2');
 
 const server = new http.Server();
 
@@ -8,15 +14,36 @@ server.on('request', (req, res) => {
   const url = new URL(req.url, `http://${req.headers.host}`);
   const pathname = url.pathname.slice(1);
 
-  const filepath = path.join(__dirname, 'files', pathname);
 
   switch (req.method) {
     case 'GET':
+      if (pathname.includes('/')) {
+        res.statusCode = HTTP_STATUS_BAD_REQUEST;
+        res.end('Bad request');
+        break;
+      }
+
+      const filepath = path.join(__dirname, 'files', pathname);
+
+      const fileReadStream = fs.createReadStream(filepath);
+
+      fileReadStream
+          .on('error', (err) => {
+            res.statusCode = err.code === 'ENOENT' ?
+            HTTP_STATUS_NOT_FOUND : HTTP_STATUS_INTERNAL_SERVER_ERROR;
+            res.end();
+          })
+          .pipe(res)
+          .on('error', () => {
+            res.statusCode = HTTP_STATUS_INTERNAL_SERVER_ERROR;
+            res.end('Internal server error');
+          });
+
+      req.on('aborted', () => fileReadStream.destroy());
 
       break;
-
     default:
-      res.statusCode = 501;
+      res.statusCode = HTTP_STATUS_NOT_IMPLEMENTED;
       res.end('Not implemented');
   }
 });
