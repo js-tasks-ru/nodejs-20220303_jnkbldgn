75e6e1c2ee727488641f2f5d403c7c0515d34493
jnkbldgn <buldygin.em@gmail.com>
2022-04-14 19:26:20 +0300

    task-5: реализована отправка сообщения для 1 подписчика

--- 05-koajs/01-chat-app/app.js
+++ 05-koajs/01-chat-app/app.js
@@ -6,12 +6,26 @@ app.use(require('koa-static')(path.join(__dirname, 'public')));
 app.use(require('koa-bodyparser')());
 
 const Router = require('koa-router');
+const {constants: {HTTP_STATUS_OK}} = require('http2');
 const router = new Router();
 
+let messageResolver;
+
 router.get('/subscribe', async (ctx, next) => {
+  const message = new Promise((resolve) => {
+    messageResolver = resolve;
+  });
+
+  const text = await message;
+
+  ctx.body = text;
+  ctx.status = HTTP_STATUS_OK;
 });
 
 router.post('/publish', async (ctx, next) => {
+  messageResolver(ctx.request.body.message);
+
+  ctx.status = HTTP_STATUS_OK;
 });
 
 app.use(router.routes());
